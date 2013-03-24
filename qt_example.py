d0ca6883bd0d0c5685c1eac2eec4271533c0bba9
Roman Haritonov <reclosedev@gmail.com>
2013-03-24 22:07:26 +0400

    playing with QProgressDialog

--- qt_example.py
+++ qt_example.py
@@ -16,6 +16,9 @@ def print_thread(message=""):
 
 
 class MainWidget(QtGui.QWidget):
+
+    progress_change = QtCore.pyqtSignal(int, int)
+
     def __init__(self, parent=None):
         super(MainWidget, self).__init__(parent)
         layout = QtGui.QVBoxLayout(self)
@@ -23,22 +26,28 @@ class MainWidget(QtGui.QWidget):
             "Start",
             clicked=partial(self.do_work,
                             "https://www.google.ru/images/srpr/logo4w.png"),
-            )
+        )
         button2 = QtGui.QPushButton(
             text="Another work",
             clicked=partial(self.do_work,
                             "http://allisonnazarian.com/wp-content/uploads/2012/02/random.jpg"),
-            )
+        )
+        button3 = QtGui.QPushButton(
+            text="With progress", clicked=self.with_progress,
+        )
         self.image_label = QtGui.QLabel()
         self.status_label = QtGui.QLabel("Status")
         self.image_result_label = QtGui.QLabel()
         hlayout = QtGui.QHBoxLayout()
         layout.addWidget(button)
         layout.addWidget(button2)
+        layout.addWidget(button3)
         layout.addWidget(self.status_label)
         hlayout.addWidget(self.image_label)
         hlayout.addWidget(self.image_result_label)
         layout.addLayout(hlayout)
+        self.progress_dialog = QtGui.QProgressDialog()
+        self.progress_change.connect(self.on_progress)
 
     @async
     def do_work(self, url, *rest):
@@ -47,13 +56,14 @@ class MainWidget(QtGui.QWidget):
         self.image_result_label.clear()
         self.status_label.setText("Loading...")
         image = yield Task(self.load_image, url)
-        pixmap = QtGui.QPixmap.fromImage(image).scaledToWidth(200)
-        self.image_label.setPixmap(pixmap)
+        pixmap = QtGui.QPixmap.fromImage(image)
+        self.image_label.setPixmap(
+            pixmap.scaledToWidth(self.image_label.width())
+        )
         self.status_label.setText("Calculating histogram...")
-        histo_image = yield Task(analyze_image, image)
+        histo_image = yield Task(self.analyze_image, image)
         self.image_result_label.setPixmap(QtGui.QPixmap.fromImage(histo_image))
         self.status_label.setText("Ready")
-
 
     def load_image(self, url):
         print "open"
@@ -62,33 +72,49 @@ class MainWidget(QtGui.QWidget):
         image = QtGui.QImage.fromData(data)
         return image
 
+    @async
+    def with_progress(self, checked):
+        result = yield Task(self.task_with_progress)
+        self.status_label.setText(result)
 
+    def on_progress(self, current, maximum):
+        self.progress_dialog.setRange(0, maximum - 1)
+        self.progress_dialog.setValue(current)
 
-def analyze_image(image, height=200):
-    print "analyze"
-    histogram = [0] * 256
-    for i in range(image.width()):
-        for j in range(image.height()):
-            color = QtGui.QColor(image.pixel(i, j))
-            histogram[color.lightness()] += 1
+    def task_with_progress(self):
+        n = 100
+        for i in range(n):
+            self.progress_change.emit(i, n)
+            if self.progress_dialog.wasCanceled():
+                self.progress_dialog.reset()
+                return "canceled"
+            time.sleep(0.05)
+        return "42"
 
-    max_value = max(histogram)
-    scale = float(height) / max_value
-    width = len(histogram)
-    print scale, width, max_value
-    histo_img = QtGui.QImage(
-        QtCore.QSize(width, height),
-        QtGui.QImage.Format_ARGB32
-    )
-    print histo_img
-    histo_img.fill(QtCore.Qt.white)
-    p = QtGui.QPainter()
-    p.begin(histo_img)
-    for i, value in enumerate(histogram):
-        y = height - int(value * scale)
-        p.drawLine(i, height, i, y)
-    p.end()
-    return histo_img
+    def analyze_image(self, image, height=200):
+        print "analyze"
+        histogram = [0] * 256
+        for i in range(image.width()):
+            for j in range(image.height()):
+                color = QtGui.QColor(image.pixel(i, j))
+                histogram[color.lightness()] += 1
+        max_value = max(histogram)
+        scale = float(height) / max_value
+        width = len(histogram)
+        print scale, width, max_value
+        histo_img = QtGui.QImage(
+            QtCore.QSize(width, height),
+            QtGui.QImage.Format_ARGB32
+        )
+        print histo_img
+        histo_img.fill(QtCore.Qt.white)
+        p = QtGui.QPainter()
+        p.begin(histo_img)
+        for i, value in enumerate(histogram):
+            y = height - int(value * scale)
+            p.drawLine(i, height, i, y)
+        p.end()
+        return histo_img
 
 
 def main():
