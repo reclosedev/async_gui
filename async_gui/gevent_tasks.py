6e28ead7904153020f6aa03b4f12a086c1b538fc
Roman Haritonov <reclosedev@gmail.com>
2013-03-25 14:17:28 +0400

    add gevent tasks draft with tests

---
+++ async_gui/gevent_tasks.py
@@ -0,0 +1,48 @@
+from concurrent import futures
+from gevent.pool import Pool
+import gevent
+
+from .engine import Task, AllTasks
+
+
+class GeventExecutor(futures.Executor):
+
+    def __init__(self, max_workers):
+        self.max_workers = max_workers
+        self._pool = Pool(max_workers)
+
+    def submit(self, fn, *args, **kwargs):
+        greenlet = self._pool.spawn(fn, *args, **kwargs)
+        return GeventFuture(greenlet)
+
+    def shutdown(self, wait=True):
+        self._pool.kill(block=wait)
+
+
+class GeventFuture(futures.Future):
+    def __init__(self, greenlet):
+        super(GeventFuture, self).__init__()
+        #self._greenlet = gevent.Greenlet()
+        self._greenlet = greenlet
+
+    def result(self, timeout=None):
+        try:
+            return self._greenlet.get(timeout=timeout)
+        except gevent.Timeout as e:
+            raise futures.TimeoutError(e)
+
+    def exception(self, timeout=None):
+        # todo timeout
+        return self._greenlet.exception
+
+    def running(self):
+        return not self._greenlet.ready()
+
+
+
+class GTask(Task):
+    executor = GeventExecutor
+
+
+class AllGTasks(AllTasks):
+    executor = GeventExecutor
---
+++ tests/gevent_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import time
+import thread
+import multiprocessing
+import urllib
+import gevent
+from gevent.monkey import patch_socket
+patch_socket()
+
+from tests.engine_test import EngineTestCase, engine, async
+from async_gui.gevent_tasks import AllGTasks
+
+
+engine.pooling_func = gevent.sleep
+
+class GeventExecutorTestCase(EngineTestCase):
+    from async_gui.gevent_tasks import GTask as Task
+
+    def test_gevent_urllib(self):
+        self.gevent_with_urllib()
+
+    @async
+    def gevent_with_urllib(self):
+
+        def download(url):
+            return urllib.urlopen(url).read()
+
+        sleep = 1
+        n = 2
+        t = time.time()
+        urls = ["http://httpbin.org/delay/%s" % sleep for _ in range(n)]
+        #result = yield AllGTasks([self.Task(download, url) for url in urls])
+        result = yield self.Task(download, urls[0])
+        print result
+        elapsed = time.time() - t
+        self.assertLess(elapsed, sleep * n)
+
+
+
+def mp_func(caller_name):
+    print multiprocessing.current_process().name, caller_name
+    return 42
+
+
+if __name__ == '__main__':
+    unittest.main()
