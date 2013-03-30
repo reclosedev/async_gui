f803443f8a6d0168776cdbc9cc850fd5e4d7ed36
Roman Haritonov <reclosedev@gmail.com>
2013-03-30 14:05:12 +0400

    rename tests files so it can be discovered

---
+++ tests/test_gevent.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+import unittest
+import time
+import urllib
+
+from gevent.monkey import patch_socket
+patch_socket()
+
+from tests.test_engine import EngineTestCase, engine, async
+from async_gui.gevent_tasks import MultiGTask
+
+
+class GeventExecutorTestCase(EngineTestCase):
+    from async_gui.gevent_tasks import GTask as Task
+    from async_gui.gevent_tasks import MultiGTask as AllTasks
+
+    testing_gevent = True
+
+    def test_gevent_urllib(self):
+        self.gevent_with_urllib()
+
+    @async
+    def gevent_with_urllib(self):
+
+        def download(url):
+            return urllib.urlopen(url).read()
+
+        delay = 1
+        n = 5
+        t = time.time()
+        urls = ["http://httpbin.org/delay/%s" % delay for _ in range(n)]
+        result = yield MultiGTask([self.Task(download, url) for url in urls])
+        result2 = yield [self.Task(download, url) for url in urls]
+        elapsed = time.time() - t
+        self.assertEqual(len(result), n)
+        self.assertLess(elapsed, delay * n)
+        print(elapsed)
+
+
+if __name__ == '__main__':
+    unittest.main()
