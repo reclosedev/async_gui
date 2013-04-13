a371a15ab358528e569a3695d7ade827e5e74542
Roman Haritonov <reclosedev@gmail.com>
2013-04-13 16:06:29 +0400

    skip gevent tests if not installed

--- tests/test_gevent.py
+++ tests/test_gevent.py
@@ -6,39 +6,41 @@ sys.path.insert(0, os.path.abspath('..'))
 import unittest
 import time
 import urllib
+try:
+    from gevent.monkey import patch_socket
+except ImportError:
+    print("Gevent not installed")
+else:
+    patch_socket()
+    from async_gui.gevent_tasks import MultiGTask
+    from tests.test_engine import EngineTestCase, engine, async
 
-from gevent.monkey import patch_socket
-patch_socket()
 
-from tests.test_engine import EngineTestCase, engine, async
-from async_gui.gevent_tasks import MultiGTask
+    class GeventExecutorTestCase(EngineTestCase):
+        from async_gui.gevent_tasks import GTask as Task
+        from async_gui.gevent_tasks import MultiGTask as AllTasks
 
+        testing_gevent = True
 
-class GeventExecutorTestCase(EngineTestCase):
-    from async_gui.gevent_tasks import GTask as Task
-    from async_gui.gevent_tasks import MultiGTask as AllTasks
+        def test_gevent_urllib(self):
+            self.gevent_with_urllib()
 
-    testing_gevent = True
+        @async
+        def gevent_with_urllib(self):
 
-    def test_gevent_urllib(self):
-        self.gevent_with_urllib()
+            def download(url):
+                return urllib.urlopen(url).read()
 
-    @async
-    def gevent_with_urllib(self):
-
-        def download(url):
-            return urllib.urlopen(url).read()
-
-        delay = 1
-        n = 5
-        t = time.time()
-        urls = ["http://httpbin.org/delay/%s" % delay for _ in range(n)]
-        result = yield MultiGTask([self.Task(download, url) for url in urls])
-        result2 = yield [self.Task(download, url) for url in urls]
-        elapsed = time.time() - t
-        self.assertEqual(len(result), n)
-        self.assertLess(elapsed, delay * n)
-        print(elapsed)
+            delay = 1
+            n = 5
+            t = time.time()
+            urls = ["http://httpbin.org/delay/%s" % delay for _ in range(n)]
+            result = yield MultiGTask([self.Task(download, url) for url in urls])
+            result2 = yield [self.Task(download, url) for url in urls]
+            elapsed = time.time() - t
+            self.assertEqual(len(result), n)
+            self.assertLess(elapsed, delay * n)
+            print(elapsed)
 
 
 if __name__ == '__main__':
